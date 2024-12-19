69e1a7a4b52c7db769083593847c57fa9813a645
Samuel Koch <samuel_koch@sluz.ch>
2024-12-19 15:53:27 +0100

    fixed tests

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ import { sum } from './index.js';
 
 describe('sum', () => {
   it('should return 3 for 1 + 2', () => {
-    assert.equal(sum(1, 2), 4);
+    assert.equal(sum(1, 2), 3);
   });
 
   it('should return 5.5 for 2.5 + 3', () => {
